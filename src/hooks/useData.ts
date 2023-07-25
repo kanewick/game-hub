dc46aa3a3c079d1d7fc8441c88e44d23eccfc512
Kalle <Kalle@KALSPC>
2023-07-26 09:22:50 +1000

    show spinner whilst fetching genres

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -10,7 +10,7 @@ interface FetchResponse<T> {
 const useData = <T>(endpoint: string) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const controller = new AbortController();
